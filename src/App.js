7e4babc6f5c0e4ec66a4ecbcff478e5094c4c15a
Weechi <Weechi@Matthews-MacBook-Air.local>
2022-03-02 18:50:56 -0500

    day2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
 
 function App() {
 
-  const [newnote, setNewnote] = useState('')
+  const [newDescription, setNewDescription] = useState('')
 
-  const hanldeNewNoteChange = (event) =>{
-    setNewNote(event.target.vaule);
+  const hanldeNewDescriptionChange = (event) =>{
+    setNewDescription(event.target.value);
   }
+
+  const handleNewDescriptionFormSubmit = (event) => {
+        event.preventDefault();
+        axios.post(
+          'http://localhost:3000/notes',
+          {
+            description:newDescription
+          }
+        )
+  }
+
+  useEffect(()=>{
+    axios
+          .get( 'http://localhost:3000/notes')
+          .then((response)=>{
+                  console.log(response);
+          })
+  },)
 
   return (
     <main>
         <h1>Noted</h1>
           <section>
             <h2>Send a positive messege to a stranger</h2>
-            <form>
+            <form onSubmit={handleNewDescriptionFormSubmit}>
                   User: <input type='text' /><br/>
-                  Note: <input type='text' onChange={hanldeNewNoteChange}/><br/>
-                  <input type='submit' vaule='Send Love' onChange={handleNewNoteFormSubmit}/>
+                  Note: <input type='text' onChange={hanldeNewDescriptionChange}/><br/>
+                  <input type='submit' value='Send Love'/>
             </form>
 
 
