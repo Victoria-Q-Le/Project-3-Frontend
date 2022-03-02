fe66a7422f2315b84c1674701ca591d2deaab1bb
Weechi <Weechi@Matthews-MacBook-Air.local>
2022-03-02 12:31:12 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [newnote, setNewnote] = useState('')
+
+  const hanldeNewNoteChange = (event) =>{
+    setNewNote(event.target.vaule);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+        <h1>Noted</h1>
+          <section>
+            <h2>Send a positive messege to a stranger</h2>
+            <form>
+                  User: <input type='text' /><br/>
+                  Note: <input type='text' onChange={hanldeNewNoteChange}/><br/>
+                  <input type='submit' vaule='Send Love' onChange={handleNewNoteFormSubmit}/>
+            </form>
+
+
+
+
+
+          </section>
+    </main>
   );
 }
 
