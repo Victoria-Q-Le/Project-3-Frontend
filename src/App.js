0c55f55d14304d3d2c8baf8a3a2ea9a3dd9f8cec
Weechi <Weechi@Matthews-MacBook-Air.local>
2022-03-03 10:02:45 -0500

    day 3 start

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from 'react';
-import axios from 'axios';
-
-
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './views/Home';
 function App() {
 
-  const [newDescription, setNewDescription] = useState('')
-
-  const hanldeNewDescriptionChange = (event) =>{
-    setNewDescription(event.target.value);
-  }
-
-  const handleNewDescriptionFormSubmit = (event) => {
-        event.preventDefault();
-        axios.post(
-          'http://localhost:3000/notes',
-          {
-            description:newDescription
-          }
-        )
-  }
-
-  useEffect(()=>{
-    axios
-          .get( 'http://localhost:3000/notes')
-          .then((response)=>{
-                  console.log(response);
-          })
-  },)
+  
 
 
   return (
-    <main>
-        <h1>Noted</h1>
-          <section>
-            <h2>Send a positive messege to a stranger</h2>
-            <form onSubmit={handleNewDescriptionFormSubmit}>
-                  User: <input type='text' /><br/>
-                  Note: <input type='text' onChange={hanldeNewDescriptionChange}/><br/>
-                  <input type='submit' value='Send Love'/>
-            </form>
-            
-          </section>
-    </main>
+    <div>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+    </div>
   );
 }
 
