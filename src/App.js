f39b424a43a534577891c82b59d1449b04213d04
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-03-07 12:59:03 -0500

    adding dashboard page to showcase basic route of frontend, home page to now used to display random message and sending new newssage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import Home from './views/Home';
 import Edit from './views/Edit';
+import Dashboard from './views/Dashboard'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/editNotes/:id' element={<Edit/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
 
         </Routes>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
       <nav>
         <h1 className='title'>Noted</h1>
+        <h1><Link className ='title' to={'/dashboard'}>Dashboard</Link></h1>
+        <h1><Link className ='title' to={'/'}>Home</Link></h1>
       </nav>
     </header>
   )
 }
 
-export default Header 
+export default Header
