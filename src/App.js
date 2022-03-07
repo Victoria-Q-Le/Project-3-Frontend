29fdcab699c482cbc01a83e6492ca1dc87a3b7f3
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-03-07 18:39:53 -0500

    changed the URL to heroku URL

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './views/Home';
 import Edit from './views/Edit';
 import Dashboard from './views/Dashboard'
+import Input from './views/Input'
 
 function App() {
 
@@ -12,7 +13,8 @@ function App() {
   return (
     <div>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Input/>}/>
+          <Route path='/home' element={<Home/>}/>
           <Route path='/editNotes/:id' element={<Edit/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,20 @@ import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <h1 className='title'>Noted</h1>
-        <h1><Link className ='title' to={'/dashboard'}>Dashboard</Link></h1>
-        <h1><Link className ='title' to={'/'}>Home</Link></h1>
+      <nav classname='navbar navbar-expand-lg navbar-light bg-light'>
+        <div className='container-fluid'>
+          <div class="navbar-brand">Note-it</div>
+          <div class="navbar-nav">
+            <div class="nav-item">
+              <Link className ='nav-link' to={'/dashboard'} style = {{textDecoration:'none'}} >Dashboard</Link>
+            </div>
+            <div class="nav-item">
+              <Link className ='nav-link' to={'/home'} style = {{textDecoration:'none'}} >Home</Link>
+            </div>
+        </div>
+        </div>
       </nav>
-    </header>
+
   )
 }
 
